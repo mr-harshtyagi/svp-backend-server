4744c56c7f028ddaf6d54be5fe360d1e7ff4d112
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-01-25 23:50:16 +0530

    feat: add working websocket server

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-// express server with routes
-import express from "express";
-import { Server } from "socket.io";
-import { createServer } from "node:http";
-
-// allow cross origin requests
-// import cors from "cors";
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-// app.use(cors());
+app.use(cors());
+const server = http.createServer(app);
+const PORT = 4000;
+const io = socketIo(server);
 
-const server = createServer(app);
-const port = 4000;
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
-// import routes
-import index from "./routes/index.js";
-// import ledOn from "./routes/led-on.js";
-// import ledOff from "./routes/led-off.js";
-// use routes
-app.use("/", index);
-
-// Define sensor data
-const sensorData = Array.from({ length: 10 }, () =>
-  Math.floor(Math.random() * 40)
-);
-
-// WebSocket server
+// Websocket connection
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.emit("sensorData", sensorData);
-  socket.on("message", (data) => {
-    console.log("message received", data);
+  console.log("A client connected");
+
+  // Handle messages from the client
+  socket.on("message", (message) => {
+    // const data = JSON.parse(message);
+    console.log("Received message data:", message);
+    // Handle the message here
+  });
+
+  socket.on("test", (message) => {
+    console.log("Received data from Raspberry Pi:", message);
+    // Handle the message here
+
+    // After handling the message, you can send a response back to the client
+    const responseData = {
+      status: "success",
+      mrValue: Math.random(),
+      smaValue: Math.random(),
+      motorSpeed: Math.random(),
+    };
+    socket.emit("testResponse", responseData);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A client disconnected");
   });
 });
 
-// app.get("/led-on", ledOn);
-// app.get("/led-off", ledOff);
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
-// start server
-server.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
