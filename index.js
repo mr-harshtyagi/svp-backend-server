4c5fd2612cc7caf8811ee8b54dde0c7a32515281
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-01-30 16:00:54 +0530

    chore: code cleanup

--- index.js
+++ index.js
@@ -54,86 +54,26 @@ io.on("connection", (socket) => {
     // socket.emit("serverMessage", responseMessage);
   });
 
+  // Handle messages from the raspberry pi
+  socket.on("raspPiMessage", (message) => {
+    console.log("Received message from client:", message);
+
+    mrValue = message.mrValue;
+    smaValue = message.smaValue;
+    motorSpeed = message.motorSpeed;
+  });
+
   // Handle disconnection
   socket.on("disconnect", () => {
     console.log("A client disconnected");
-    // clearInterval(dataInterval); // Stop sending data when the client disconnects
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Place your route definitions here, after the CORS middleware
 app.get("/", (req, res) => {
   res.send("SVP server is online!");
 });
-
-//Harsh's code
-
-// const express = require("express");
-// const http = require("http");
-// const socketIo = require("socket.io");
-// const cors = require("cors");
-
-// const app = express();
-// app.use(cors());
-// const server = http.createServer(app);
-// const PORT = 4000;
-// const io = socketIo(server);
-
-// // Websocket connection
-// io.on("connection", (socket) => {
-//   console.log("A client connected");
-
-//   // Handle messages from the client
-//   socket.on("message", (message) => {
-//     // const data = JSON.parse(message);
-//     console.log("Received message data:", message);
-//     // Handle the message here
-//   });
-
-//   // Handle messages from the client
-//   socket.on("data", (message) => {
-//     // const data = JSON.parse(message);
-//     console.log("Received message data:", message);
-
-//     // After handling the message, you can send a response back to the client
-//     const responseData = {
-//       status: "success",
-//       mrValue: Math.random(),
-//       smaValue: Math.random(),
-//       motorSpeed: Math.random(),
-//     };
-//     socket.emit("dataResponse", responseData);
-//     // Handle the message here
-//   });
-
-//   socket.on("test", (message) => {
-//     console.log("Received data from Raspberry Pi:", message);
-//     // Handle the message here
-
-//     // After handling the message, you can send a response back to the client
-//     const responseData = {
-//       status: "success",
-//       mrValue: Math.random(),
-//       smaValue: Math.random(),
-//       motorSpeed: Math.random(),
-//     };
-//     socket.emit("testResponse", responseData);
-//   });
-
-//   // Handle disconnection
-//   socket.on("disconnect", () => {
-//     console.log("A client disconnected");
-//   });
-// });
-
-// server.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// Path: routes/index.js
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
-});
-
-export default router;
